c1d41a945fa7e3d1dc1a4aff8af42d778f71d2dc
Ragavendira Rajathirumalai <ragavendira.rajathirumalai@expleogroup.com>
2022-03-10 15:35:35 +0530

    added Modules

---
+++ azure/modules/vm/variables.tf
@@ -0,0 +1,63 @@
+variable "resource_group_name" {
+  type        = string
+  description = "Name of resource group to deploy instance into"
+  default = "jenkins-resources"
+}
+
+variable "location" {
+  type        = string
+  description = "Azure location to deploy instance into"
+  default     = "West US 2"
+}
+
+variable "subnet" {
+  type        = string
+  description = "Subnet to deploy instance into"
+  
+}
+
+variable "prefix" {
+  type        = string
+  description = "Prefix to place before deployed resources"
+  default     = "jenkins"
+}
+
+variable "admin_user" {
+  type        = string
+  description = "Admin user for the image we're launching"
+}
+
+variable "admin_key_public" {
+  type        = string
+  description = "Public SSH key of admin user"
+}
+
+variable "instance_type" {
+  type        = string
+  description = "Instance type/size to deploy"
+  default     = "Standard_B2ms"
+}
+
+variable "client_id" {
+  type        = string
+  description = "Azure Client ID used for authentication"
+  default = "3b9b6238-3e95-4529-abf3-6e7cfeaf30a5"
+}
+
+variable "client_secret" {
+  type        = string
+  description = "Azure Client Secret used for authentication"
+  default = "ZB75cg_rwN9-Cj5VDkJHOA8LotV5z1~SMD"
+}
+
+variable "subscription_id" {
+  type        = string
+  description = "Azure Subscription ID used for authentication"
+  default = "39716e65-c90e-45f6-b876-fda4859c807d"
+}
+
+variable "tenant_id" {
+  type        = string
+  description = "Azure Tenant ID used for authentication"
+  default = "3b0e7247-e0d5-44bf-8ed1-d01b18d16ca2"
+}
